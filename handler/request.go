253cf2b40fea66d798d74f8122a062512718ca4e
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-10-16 12:37:55 -0700

    feat: add user request

---
+++ handler/request.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"github.com/gebhartn/impress/model"
+	"github.com/gofiber/fiber/v2"
+)
+
+type userRegisterRequest struct {
+	User struct {
+		Username string `json:"username" validate:"required"`
+		Password string `json:"password" validate:"required"`
+	} `json:"user"`
+}
+
+func (r *userRegisterRequest) bind(c *fiber.Ctx, u *model.User, v *Validator) error {
+
+	if err := c.BodyParser(r); err != nil {
+		return err
+	}
+
+	if err := v.Validate(r); err != nil {
+		return err
+	}
+
+	u.Username = r.User.Username
+	hash, err := u.HashPassword(r.User.Password)
+	if err != nil {
+		return err
+	}
+	u.Password = hash
+
+	return nil
+}
