e451dee213662dad3d5ae9a93296bef421fa689d
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-10-16 13:32:49 -0700

    feat: register user route

--- handler/routes.go
+++ handler/routes.go
@@ -1,21 +1,12 @@
 package handler
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/gofiber/fiber/v2"
 )
 
 func (h *Handler) Register(r *fiber.App) {
 	v1 := r.Group("/api")
 
-	hello := v1.Group("/:name")
-	hello.Get("", h.SayHello)
-}
-
-func (h *Handler) SayHello(c *fiber.Ctx) error {
-	name := c.Params("name", "world")
-	res := map[string]interface{}{"res": fmt.Sprintf("Hello, %s!", name)}
-	return c.Status(http.StatusOK).JSON(res)
+	guests := v1.Group("/users")
+	guests.Post("", h.SignUp)
 }
