cc2b0ef75fdf1d3b71034fd267521513c09c0079
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-10-16 12:19:13 -0700

    feat: router

---
+++ router/router.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func NewRouter() *fiber.App {
+	r := fiber.New()
+	r.Use(logger.New())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+		AllowMethods: "POST, GET, PUT, DELETE, HEAD",
+	}))
+
+	return r
+}
