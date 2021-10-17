e8d07802d9f1baaceb1e60b07599c57f24813964
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-10-17 14:55:52 -0700

    feat: annotate default public

--- model/image.go
+++ model/image.go
@@ -5,7 +5,7 @@ import "github.com/jinzhu/gorm"
 type Image struct {
 	gorm.Model
 	Owner     User
-	Public    bool
+	Public    bool   `gorm:"default:false"`
 	Favorites []User `gorm:"many2many:favorites;"`
 }
 
