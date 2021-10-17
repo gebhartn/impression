f6221a881bd3ce0b61c18d2ed0d71876cd9a50c3
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-10-17 14:56:39 -0700

    feat: file upload handler

---
+++ handler/image.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gebhartn/impress/model"
+	"github.com/gebhartn/impress/utils"
+	"github.com/gofiber/fiber/v2"
+)
+
+func (h *Handler) UploadFile(c *fiber.Ctx) error {
+	var i model.Image
+	r := &imageUploadRequest{}
+
+	if err := r.bind(c, &i, h.validator); err != nil {
+		return c.Status(http.StatusUnprocessableEntity).JSON(utils.NewError(err))
+	}
+
+	_, err := h.s3.UploadObject(r.Image.Owner, r.Image.File)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
