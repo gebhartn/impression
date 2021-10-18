32d26d9096872ae108a935615b459cdc295db56c
Nicholas Gebhart <nicholas.gebhart@gmail.com>
2021-10-17 18:26:50 -0700

    fix: interface signature

--- s3/s3.go
+++ s3/s3.go
@@ -4,12 +4,11 @@ import (
 	"mime/multipart"
 
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 type Store interface {
 	ListBuckets() (*s3.ListBucketsOutput, error)
 	ListObjects() (*s3.ListObjectsV2Output, error)
 	ListObjectsById(id uint) (*s3.ListObjectsV2Output, error)
-	UploadObject(id uint, f *multipart.FileHeader) (*s3manager.UploadOutput, error)
+	UploadObject(id uint, f *multipart.FileHeader) (string, error)
 }
